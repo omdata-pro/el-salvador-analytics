7f156b7683338aedc670b37913a2ee5da8a1c816
Orquidea Mohammed <omdata.analyst@gmail.com>
2026-08-14 00:07:23 -0500

    Committing 1 item from workspace 54e503e8-ffac-4107-9d85-2a67491b6def

---
+++ 20_Gold_Aggregations.Notebook/notebook-content.py
@@ -0,0 +1,37 @@
+# Fabric notebook source
+
+# METADATA ********************
+
+# META {
+# META   "kernel_info": {
+# META     "name": "synapse_pyspark"
+# META   },
+# META   "dependencies": {
+# META     "lakehouse": {
+# META       "default_lakehouse": "447fcbb5-1f3d-4bcb-a966-6915930db478",
+# META       "default_lakehouse_name": "ElSalvador_02_Silver",
+# META       "default_lakehouse_workspace_id": "54e503e8-ffac-4107-9d85-2a67491b6def",
+# META       "known_lakehouses": [
+# META         {
+# META           "id": "447fcbb5-1f3d-4bcb-a966-6915930db478"
+# META         },
+# META         {
+# META           "id": "06bd0ffa-add5-40a5-9f6d-b81355c6e8ad"
+# META         }
+# META       ]
+# META     }
+# META   }
+# META }
+
+# CELL ********************
+
+# Welcome to your new notebook
+# Type here in the cell editor to add code!
+
+
+# METADATA ********************
+
+# META {
+# META   "language": "python",
+# META   "language_group": "synapse_pyspark"
+# META }
